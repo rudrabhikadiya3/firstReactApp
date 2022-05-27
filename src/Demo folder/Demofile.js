2ec5562fcf7f0eb6dcb1d906117f39a6163578ff
rudrabhikadiya3 <rudra.rnw@gmail.com>
2022-05-27 15:31:14 +0530

    27 new component

---
+++ src/Demo folder/Demofile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Demofile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World, this is first Component</h1>
+
+
+
+            </div>
+        );
+    }
+}
+
+export default Demofile;
