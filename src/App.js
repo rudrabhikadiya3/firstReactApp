2741c48b7c0fc14e1589244d78b548ec832d554d
Rudra Bhikadiya <99029749+rudrabhikadiya3@users.noreply.github.com>
2022-05-31 18:31:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 import Demofile from './Demo folder/Demofile';
 
+let medicine = [
+  {
+    id: 101,
+    name: 'Abacavir',
+    quantity: 25,
+    price: 150,
+    expiry: 2022,
+    status: true
+  },
+  {
+    id: 102,
+    name: 'Eltrombopag',
+    quantity: 90,
+    price: 550,
+    expiry: 2021,
+    status: true
+  },
+  {
+    id: 103,
+    name: 'Meloxicam',
+    quantity: 85,
+    price: 450,
+    expiry: 2025,
+    status: false
+  },
+  {
+    id: 104,
+    name: 'Allopurinol',
+    quantity: 50,
+    price: 600,
+    expiry: 2023,
+    status: true
+  },
+  {
+    id: 105,
+    name: 'Phenytoin',
+    quantity: 63,
+    price: 250,
+    expiry: 2021,
+    status: false
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <Demofile/>
-    </div>
+    <table border="1">
+      <thead>
+        <tr >
+          <th>ID</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Expiry</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      {
+        medicine.map((v) => {
+          let { id, name, quantity, price, expiry, status } = v;
+          return (
+            <tr align="center">
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{quantity}</td>
+              <td>{price}</td>
+              <td>{expiry}</td>
+              <td>{status.toString()}</td>
+            </tr>
+          )
+        })
+      }
+    </table>
   );
 }
 
