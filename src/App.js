889415b31a084e81e031118915089670566f60e7
rudrabhikadiya3 <rudra.rnw@gmail.com>
2022-06-01 16:01:11 +0530

    emp init

--- src/App.js
+++ src/App.js
@@ -2,53 +2,97 @@ import logo from './logo.svg';
 import './App.css';
 import Demofile from './Demo folder/Demofile';
 
-let medicine = [
-  {
-    id: 101,
-    name: 'Abacavir',
-    quantity: 25,
-    price: 150,
-    expiry: 2022,
-    status: true
-  },
-  {
-    id: 102,
-    name: 'Eltrombopag',
-    quantity: 90,
-    price: 550,
-    expiry: 2021,
-    status: true
-  },
-  {
-    id: 103,
-    name: 'Meloxicam',
-    quantity: 85,
-    price: 450,
-    expiry: 2025,
-    status: false
-  },
-  {
-    id: 104,
-    name: 'Allopurinol',
-    quantity: 50,
-    price: 600,
-    expiry: 2023,
-    status: true
-  },
-  {
-    id: 105,
-    name: 'Phenytoin',
-    quantity: 63,
-    price: 250,
-    expiry: 2021,
-    status: false
-  }
-];
+
 
 function App() {
+  let medicine = [
+    {
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
+    },
+    {
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+  ]
+
+  let empData = [
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
+
+
+  let medicineFilter = medicine.filter((d, i) => d.status === true && d.expiry >= 2022)
+  let total_price = medicineFilter.reduce((acc, d, i) => acc + d.price, 0)
   return (
-    <table border="1">
-      <thead>
+    <div>
+      <table border="1">
+
         <tr >
           <th>ID</th>
           <th>Name</th>
@@ -56,25 +100,46 @@ function App() {
           <th>Price</th>
           <th>Expiry</th>
           <th>Status</th>
+          <th>total price</th>
         </tr>
-      </thead>
-      {
-        medicine.map((v) => {
-          let { id, name, quantity, price, expiry, status } = v;
-          return (
-            <tr align="center">
-              <td>{id}</td>
-              <td>{name}</td>
-              <td>{quantity}</td>
-              <td>{price}</td>
-              <td>{expiry}</td>
-              <td>{status.toString()}</td>
-            </tr>
-          )
-        })
-      }
-    </table>
+
+
+        {
+          medicineFilter.map((v, i) => {
+            let { id, name, quantity, price, expiry, status } = v;
+            return (
+              <tr key={i}>
+                <td >{id}</td>
+                <td>{name}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+                <td>{expiry}</td>
+                <td>{status.toString()}</td>
+                {i === 0 ? <td rowSpan="2">{total_price}</td> : null}
+              </tr>
+            )
+          })
+        }
+      </table>
+      <table border="1">
+
+        <tr >
+          <th>ID</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Expiry</th>
+          <th>Status</th>
+          <th>total price</th>
+        </tr>
+      </table>
+
+    </div>
+
+
+
   );
+
 }
 
 export default App;
